f13d1515db30e1ea45ef7969c3ac4c245c7f482e
isaacrochaoliveira <isaacvroliveira@gmail.com>
2025-05-30 23:18:41 -0300

    Update ministerio-louvor.php

--- JChurch/painel/ministerio-louvor.php
+++ JChurch/painel/ministerio-louvor.php
@@ -3,7 +3,7 @@ require_once('../index_php/protect.php');
 require_once('../config/conect.php');
 ?>
 <style>
-    section {
+    section#header {
         background: linear-gradient(rgba(0, 0, 0, .8), rgba(0, 0, 0, 0.8)), url('imagens/celebration.jpg') no-repeat;
         padding: 250px 150px;
         background-position: center;
@@ -19,4 +19,9 @@ require_once('../config/conect.php');
 <section id="header">
     <h1>Ministério de Louvor</h1>
     <p><?php echo "\u{1F3B6}"?> <?php echo "\u{1F3B9}"?> <?php echo "\u{1F3BA}"?></p>
+</section>
+<section class="container py-5">
+    <div>
+        <h3>Integrantes do Ministério: </h3>
+    </div>
 </section>
